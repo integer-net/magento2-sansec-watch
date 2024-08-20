00fbeb38bfc80c2c1031941d39c8e74cf6e44c13
Julian Nuß <jn@integer-net.de>
2024-08-20 18:00:35 +0200

    #34994 Refactor handling of FPC

---
+++ tests/Service/UpdateFpcTest.php
@@ -0,0 +1,114 @@
+<?php
+/** @noinspection DuplicatedCode */
+
+declare(strict_types=1);
+
+namespace IntegerNet\SansecWatch\Test\Service;
+
+use IntegerNet\SansecWatch\Model\Config;
+use IntegerNet\SansecWatch\Model\FpcMode;
+use IntegerNet\SansecWatch\Service\UpdateFpc;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\PageCache\Model\Config as PageCacheConfig;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\MockObject\Stub;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(UpdateFpc::class)]
+class UpdateFpcTest extends TestCase
+{
+    private Config&MockObject $config;
+    private PageCacheConfig&Stub $pageCacheConfig;
+    private TypeListInterface&MockObject $cacheTypeList;
+
+    private UpdateFpc $updateFpc;
+
+    /**
+     * @noinspection PhpUnhandledExceptionInspection
+     */
+    protected function setUp(): void
+    {
+        $this->config          = self::createMock(Config::class);
+        $this->pageCacheConfig = self::createStub(PageCacheConfig::class);
+        $this->cacheTypeList   = self::createMock(TypeListInterface::class);
+
+        $this->updateFpc = new UpdateFpc(
+            $this->config,
+            $this->pageCacheConfig,
+            $this->cacheTypeList,
+        );
+    }
+
+    #[Test]
+    public function nothingIsDoneIfFpcIsDisabled(): void
+    {
+        $this->pageCacheConfig
+            ->method('isEnabled')
+            ->willReturn(false);
+
+        $this->config
+            ->expects(self::never())
+            ->method(self::anything());
+
+        $this->updateFpc->execute();
+    }
+
+    #[Test]
+    public function nothingIsDoneIfFpcModeIsNone(): void
+    {
+        $this->pageCacheConfig
+            ->method('isEnabled')
+            ->willReturn(true);
+
+        $this->config
+            ->expects(self::once())
+            ->method('getFpcMode')
+            ->willReturn(FpcMode::NONE);
+
+        $this->cacheTypeList
+            ->expects(self::never())
+            ->method(self::anything());
+
+        $this->updateFpc->execute();
+    }
+
+    #[Test]
+    public function invalidatesFpcIfModeIfInvalidate(): void
+    {
+        $this->pageCacheConfig
+            ->method('isEnabled')
+            ->willReturn(true);
+
+        $this->config
+            ->expects(self::once())
+            ->method('getFpcMode')
+            ->willReturn(FpcMode::INVALIDATE);
+
+        $this->cacheTypeList
+            ->expects(self::once())
+            ->method('invalidate');
+
+        $this->updateFpc->execute();
+    }
+
+    #[Test]
+    public function clearsFpcIfModeIfClear(): void
+    {
+        $this->pageCacheConfig
+            ->method('isEnabled')
+            ->willReturn(true);
+
+        $this->config
+            ->expects(self::once())
+            ->method('getFpcMode')
+            ->willReturn(FpcMode::CLEAR);
+
+        $this->cacheTypeList
+            ->expects(self::once())
+            ->method('cleanType');
+
+        $this->updateFpc->execute();
+    }
+}
